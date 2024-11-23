9adf5212684e6e14252e3cc3f103327b914a173c
KangKung <87695599+KangKung02@users.noreply.github.com>
2024-11-23 15:10:25 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/royxGameLoader/refs/heads/main/ARkaitun.lua"))();
+if getgenv().KAITUN_CONFIG then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/royxGameLoader/refs/heads/main/ARkaitun.lua"))();
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/royxGameLoader/refs/heads/main/ARmain.lua"))();
+end
